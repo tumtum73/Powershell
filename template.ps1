deda47a7a717720948499299276376f916cb10ed
Stephen Correia <tumtum73@gmail.com>
2020-04-11 22:18:02 -0400

    Create template.ps1
    Initial script

---
+++ template.ps1
@@ -0,0 +1,37 @@
+<#
+.SYNOPSIS
+<synopsis of script>
+
+.DESCRIPTION
+<description>
+
+.PARAMETER <parameter1>
+<description of parameter1>
+
+.PARAMETER <parameter2>
+<description of parameter2>
+
+
+.EXAMPLE
+ <example of script in action>
+
+.EXAMPLE
+ <example of script in action>
+
+.NOTES
+ <unique notes for script>
+	Script: 	<script name>
+	Version: 	1.0
+	Author: 	Stephen Correia
+	Date: 		MM/DD/YYYY
+	Keywords:	
+	Comments:	
+	References:
+	<url to original concept for reference>
+#>
+
+#Functions
+
+#Main
+
+#End of Script
