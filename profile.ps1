b40448f85e20b361f422b238b00ea246d0438bc8
Stephen Correia <tumtum73@gmail.com>
2025-12-03 17:47:11 -0500

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,5 +1,7 @@
 $DaysToXmas = (New-TimeSpan -Start (Get-Date) -End "12/25/$((Get-Date).Year)").Days
-$DaysToBday = (New-TimeSpan -Start (Get-Date) -End "08/22/$((Get-Date).Year)").Days
+
+if ((Get-Date).Month -gt 8) {($DaysToBday = (New-TimeSpan -Start (Get-Date) -End "08/22/$((Get-Date).Year+1)").Days)}
+  else {$DaysToBday = (New-TimeSpan -Start (Get-Date) -End "08/22/$((Get-Date).Year)").Days}
 
 Write-Host -Foreground Yellow "$DaysToXmas days until Christmas!"
 Write-Host -Foreground Yellow "$DaysToBday days until My Birthday!"
