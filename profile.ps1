2035ca3389f2aa4dae2789247bfda5aa089c6ae7
Stephen Correia <tumtum73@gmail.com>
2025-12-03 18:06:43 -0500

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,7 +1,9 @@
 $DaysToXmas = (New-TimeSpan -Start (Get-Date) -End "12/25/$((Get-Date).Year)").Days
 
-if ((Get-Date).Month -gt 8) {($DaysToBday = (New-TimeSpan -Start (Get-Date) -End "08/22/$((Get-Date).Year+1)").Days)}
-  else {$DaysToBday = (New-TimeSpan -Start (Get-Date) -End "08/22/$((Get-Date).Year)").Days}
+# Determine if todays daye is after my birthday or not, to accurately display the days until my birthday.
+$myBday = "8/22/"
+if ((Get-Date) -gt (Get-Date -Month 8 -Day 22)) {$DaysToBday = (New-TimeSpan -Start (Get-Date) -End "$myBday$((Get-Date).Year+1)").Days} 
+  else {$DaysToBday = (New-TimeSpan -Start (Get-Date) -End "$myBday$((Get-Date).Year)").Days}
 
 Write-Host -Foreground Yellow "$DaysToXmas days until Christmas!"
 Write-Host -Foreground Yellow "$DaysToBday days until My Birthday!"
